de693b9240e155a5cd2ae004fda194d140f6487c
tu_tu <1012090981@qq.com>
2021-04-26 12:04:41 +0800

    路径参数和查询参数

--- gin/demo01/main.go
+++ gin/demo01/main.go
@@ -1,11 +1,32 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"fmt"
+	"net/http"
+	"strings"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
 	r := gin.Default()
 	r.GET("/", func(c *gin.Context) {
 		c.String(200, "Hello, Geektutu")
+	})
+
+	//API参数，获得路径上的信息
+	r.GET("/user/:name/*action", func(c *gin.Context) {
+		name := c.Param("name")
+		action := c.Param("action")
+		action = strings.Trim(action, "/")
+		c.String(http.StatusOK, name+" is "+action)
+	})
+
+	//Url参数，获得路径后跟随的参数
+	r.GET("/user", func(c *gin.Context) {
+		//默认值为tutu，/user?name=changchang的话，值为changchang
+		name := c.DefaultQuery("name", "tutu")
+		c.String(http.StatusOK, fmt.Sprintf("hello %s", name))
 	})
 	r.Run()
 }
