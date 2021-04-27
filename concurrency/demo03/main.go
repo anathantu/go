3835c31ee4b834f4a6b2ade29fdea8f04a1157ed
tu_tu <1012090981@qq.com>
2021-04-27 11:40:52 +0800

    单向channel

--- concurrency/demo03/main.go
+++ concurrency/demo03/main.go
@@ -5,7 +5,28 @@ import (
 	"runtime"
 )
 
+func counter(out chan<- int) {
+	for i := 0; i < 100; i++ {
+		out <- i
+	}
+	close(out)
+}
+
+func squarer(out chan<- int, in <-chan int) {
+	for i := range in {
+		out <- i * i
+	}
+	close(out)
+}
+
+func printer(in <-chan int) {
+	for i := range in {
+		fmt.Println(i)
+	}
+}
+
 func main() {
+
 	go func(s string) {
 		for i := 0; i < 2; i++ {
 			fmt.Println(s)
@@ -16,4 +37,10 @@ func main() {
 		runtime.Gosched()
 		fmt.Println("hello")
 	}
+
+	ch1 := make(chan int)
+	ch2 := make(chan int)
+	go counter(ch1)
+	go squarer(ch2, ch1)
+	printer(ch2)
 }
