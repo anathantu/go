436c68be2e8cede6be28aedc23906bc9c8b1c1a1
tu_tu <1012090981@qq.com>
2021-04-28 17:51:21 +0800

    协程的使用

--- concurrency/demo04/main.go
+++ concurrency/demo04/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"time"
 )
 
 func recv(c chan int) {
@@ -10,8 +11,23 @@ func recv(c chan int) {
 }
 
 func main() {
+	done := make(chan bool)
+	go func() {
+		for i := 0; i < 3; i++ {
+			time.Sleep(100 * time.Millisecond)
+			fmt.Println("hello world")
+		}
+		done <- true
+	}()
+
+	<-done //通道会阻塞
+	//通过这样的方式，可以实现，主协程等待前一个协程执行结束的效果
+	fmt.Println("over!")
+
 	ch := make(chan int)
 	go recv(ch)
-	ch <- 10
+	var a int
+	fmt.Scanln(&a)
+	ch <- a
 	fmt.Print("success")
 }
