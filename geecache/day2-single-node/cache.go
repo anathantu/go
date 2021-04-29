772c14077b57910f8b305e4ebe81837bbaa17c60
tu_tu <1012090981@qq.com>
2021-04-29 22:02:48 +0800

    完成了并发缓存

--- geecache/day2-single-node/cache.go
+++ geecache/day2-single-node/cache.go
@@ -19,3 +19,20 @@ func (c *cache) add(key string, value ByteView) {
 	}
 	c.lru.Add(key, value)
 }
+
+func (c *cache) get(key string) (value ByteView, ok bool) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	if c.lru == nil {
+		return
+	}
+
+	if v, ok := c.lru.Get(key); ok {
+		//这里的v.(ByteView)属于接口转换成类型，
+		//基本格式是：t := i.(T)
+		//i 代表接口变量，T 代表转换的目标类型，t 代表转换后的变量。
+		return v.(ByteView), ok
+	}
+
+	return
+}
