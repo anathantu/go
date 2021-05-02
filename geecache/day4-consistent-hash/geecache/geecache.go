d35f9abe1d2432d4124262fbc777cc178a9d4895
tu_tu <1012090981@qq.com>
2021-05-02 16:07:46 +0800

    一致性hash

---
+++ geecache/day4-consistent-hash/geecache/geecache.go
@@ -0,0 +1,90 @@
+package geecache
+
+import (
+	"fmt"
+	"log"
+	"sync"
+)
+
+/**
+*一个 Group 可以认为是一个缓存的命名空间，每个 Group 拥有一个唯一的名称 name。
+*比如可以创建三个 Group，缓存学生的成绩命名为 scores，缓存学生信息的命名为 info，
+*缓存学生课程的命名为 courses。
+ */
+
+type Group struct {
+	name      string
+	getter    Getter
+	mainCache cache
+}
+
+type Getter interface {
+	Get(key string) ([]byte, error)
+}
+
+//定义了一个函数类型
+type GetterFunc func(key string) ([]byte, error)
+
+func (f GetterFunc) Get(key string) ([]byte, error) {
+	return f(key)
+}
+
+var (
+	mu     sync.RWMutex
+	groups = make(map[string]*Group)
+)
+
+func NewGroup(name string, cacheBytes int64, getter Getter) *Group {
+	if getter == nil {
+		panic("nil Getter")
+	}
+	mu.Lock()
+	defer mu.Unlock()
+	g := &Group{
+		name:      name,
+		getter:    getter,
+		mainCache: cache{cacheBytes: cacheBytes},
+	}
+	groups[name] = g
+	return g
+}
+
+func GetGroup(key string) *Group {
+	mu.RLock()
+	g := groups[key]
+	mu.RUnlock()
+	return g
+}
+
+func (g *Group) Get(key string) (ByteView, error) {
+	if key == "" {
+		return ByteView{}, fmt.Errorf("key is required")
+	}
+
+	if v, ok := g.mainCache.get(key); ok {
+		log.Println("[GeeCache] hit")
+		return v, nil
+	}
+
+	//没有命中缓存时加载缓存
+	return g.load(key)
+}
+
+func (g *Group) load(key string) (value ByteView, err error) {
+	return g.getLocally(key)
+}
+
+func (g *Group) getLocally(key string) (ByteView, error) {
+	//调用Group中的回调函数
+	bytes, err := g.getter.Get(key)
+	if err != nil {
+		return ByteView{}, err
+	}
+	value := ByteView{b: cloneBytes(bytes)}
+	g.populateCache(key, value)
+	return value, nil
+}
+
+func (g *Group) populateCache(key string, value ByteView) {
+	g.mainCache.add(key, value)
+}
---
+++ geecache/day4-consistent-hash/geecache/http.go
@@ -0,0 +1,63 @@
+package geecache
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"strings"
+)
+
+const defaultBasePath = "/_geecache/"
+
+/*
+	分布式缓存需要实现节点间通信，采用的是基于HTTP协议的方案
+*/
+
+type HTTPPool struct {
+	//self 用来记录自己的ip地址，basePath作为节点间通讯的前缀
+	self     string
+	basePath string
+}
+
+func NewHTTPPool(self string) *HTTPPool {
+	return &HTTPPool{
+		self:     self,
+		basePath: defaultBasePath,
+	}
+}
+
+func (p *HTTPPool) Log(format string, v ...interface{}) {
+	log.Printf("[Server %s] %s", p.self, fmt.Sprintf(format, v...))
+}
+
+func (p *HTTPPool) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	if !strings.HasPrefix(r.URL.Path, p.basePath) {
+		panic("HTTPPool serving unexpected path:" + r.URL.Path)
+	}
+	p.Log("%s %s", r.Method, r.URL.Path)
+	//默认采用的格式是/<basepath>/<groupname>/<key>
+	//Path[len(p.basePath):]是 string的一种类似切片的做法
+	parts := strings.SplitN(r.URL.Path[len(p.basePath):], "/", 2)
+	if len(parts) != 2 {
+		http.Error(w, "bad request", http.StatusBadRequest)
+		return
+	}
+
+	groupName := parts[0]
+	key := parts[1]
+
+	group := GetGroup(groupName)
+	if group == nil {
+		http.Error(w, "no such group:"+groupName, http.StatusNotFound)
+		return
+	}
+
+	view, err := group.Get(key)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/octet-stream")
+	w.Write(view.ByteSlice())
+}
