8169fe69067b1653aa754337312b45db1ce17bb4
tu_tu <1012090981@qq.com>
2021-05-04 19:01:34 +0800

    分布式节点

---
+++ geecache/day5-multi-nodes/geecache/lru/lru.go
@@ -0,0 +1,71 @@
+package lru
+
+import "container/list"
+
+type Cache struct {
+	maxBytes  int64
+	nowbytes  int64
+	list      *list.List
+	cache     map[string]*list.Element
+	OnEvicted func(key string, value Value)
+}
+
+type entry struct {
+	key   string
+	value Value
+}
+
+type Value interface {
+	Len() int
+}
+
+func New(maxBytes int64, onEvicted func(key string, value Value)) *Cache {
+	return &Cache{
+		maxBytes:  maxBytes,
+		list:      list.New(),
+		cache:     make(map[string]*list.Element),
+		OnEvicted: onEvicted,
+	}
+}
+
+func (c *Cache) Get(key string) (value Value, ok bool) {
+	if ele, ok := c.cache[key]; ok {
+		c.list.MoveToFront(ele) //移到队尾
+		kv := ele.Value.(*entry)
+		return kv.value, true
+	}
+	return
+}
+
+func (c *Cache) RemoveOldest() {
+	ele := c.list.Back()
+	if ele != nil {
+		c.list.Remove(ele)
+		kv := ele.Value.(*entry)
+		delete(c.cache, kv.key)
+		c.nowbytes -= int64(len(kv.key)) + int64(kv.value.Len())
+		if c.OnEvicted != nil {
+			c.OnEvicted(kv.key, kv.value)
+		}
+	}
+}
+
+func (c *Cache) Add(key string, value Value) {
+	if ele, ok := c.cache[key]; ok {
+		c.list.MoveToFront(ele)
+		kv := ele.Value.(*entry)
+		c.nowbytes += int64(value.Len()) - int64(kv.value.Len())
+		kv.value = value
+	} else {
+		ele := c.list.PushFront(&entry{key, value})
+		c.cache[key] = ele
+		c.nowbytes += int64(len(key)) + int64(value.Len())
+	}
+	for c.maxBytes != 0 && c.maxBytes < c.nowbytes {
+		c.RemoveOldest()
+	}
+}
+
+func (c *Cache) Len() int {
+	return c.list.Len()
+}
