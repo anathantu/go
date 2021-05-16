1d15421295f2b1fa04e0436c65d4dd62036a1bd7
tu_tu <1012090981@qq.com>
2021-05-16 15:10:07 +0800

    go语言反射

---
+++ reflect/reflect.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"fmt"
+	"reflect"
+)
+
+type Enum int
+
+const (
+	Zero Enum =0
+)
+
+func test01(){
+	type cat struct{}
+
+	ins:=&cat{}
+
+	typeOfCat:=reflect.TypeOf(ins)
+	// 显示反射类型对象的名称和种类
+	fmt.Printf("name:'%v' kind:'%v'\n", typeOfCat.Name(), typeOfCat.Kind())
+	// 取类型的元素
+	typeOfCat = typeOfCat.Elem()
+	// 显示反射类型对象的名称和种类
+	fmt.Printf("element name: '%v', element kind: '%v'\n", typeOfCat.Name(), typeOfCat.Kind())
+	// 获取Zero常量的反射类型对象
+	typeOfA := reflect.TypeOf(Zero)
+	// 显示反射类型对象的名称和种类
+	fmt.Println(typeOfA.Name(), typeOfA.Kind())
+}
+
+func test2(){
+	// 声明一个空结构体
+	type cat struct {
+		Name string
+		// 带有结构体tag的字段
+		Type int `json:"type" id:"100"`
+	}
+	// 创建cat的实例
+	ins := cat{Name: "mimi", Type: 1}
+	// 获取结构体实例的反射类型对象
+	typeOfCat := reflect.TypeOf(ins)
+	// 遍历结构体所有成员
+	for i := 0; i < typeOfCat.NumField(); i++ {
+		// 获取每个成员的结构体字段类型
+		fieldType := typeOfCat.Field(i)
+		// 输出成员名和tag
+		fmt.Printf("name: %v  tag: '%v'\n", fieldType.Name, fieldType.Tag)
+	}
+	// 通过字段名, 找到字段类型信息
+	if catType, ok := typeOfCat.FieldByName("Type"); ok {
+		// 从tag中取出需要的tag
+		fmt.Println(catType.Tag.Get("json"), catType.Tag.Get("id"))
+	}
+}
+
+func main(){
+	test2()
+}
